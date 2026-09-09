c06ffb2c5e29d47e77cb6173fcfe8f82571d8cdc
DarthRomolus <rom.mail2005@gmail.com>
2026-09-09 11:21:52 +0300

    hotfix

--- senders/go.mod
+++ senders/go.mod
@@ -5,10 +5,8 @@ go 1.27.0
 require (
 	github.com/klauspost/reedsolomon v1.14.2
 	golang.org/x/exp v0.0.0-20260824195058-e88cd73687aa
+	golang.org/x/sys v0.30.0
 	google.golang.org/protobuf v1.36.12
 )
 
-require (
-	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
-	golang.org/x/sys v0.30.0 // indirect
-)
+require github.com/klauspost/cpuid/v2 v2.3.0 // indirect
--- senders/internal/counter/counter.go
+++ senders/internal/counter/counter.go
@@ -5,39 +5,36 @@ import (
 	"fmt"
 	"os"
 
-	"golang.org/x/sys/windows"
+	"golang.org/x/sys/unix"
 )
 
-func Count(counterFile *os.File)(uint64,error){
+func Count(counterFile *os.File) (uint64, error) {
 
-	h := windows.Handle(counterFile.Fd())
-	var ol windows.Overlapped
-
-
-	err := windows.LockFileEx(h, 2, 0, 8, 0, &ol)
+	err := unix.Flock(int(counterFile.Fd()), unix.LOCK_EX)
 	if err != nil {
 		return 0, fmt.Errorf("failed to lock file: %w", err)
 	}
-	defer windows.UnlockFileEx(h, 0, 8, 0, &ol)
-	buf:=make([]byte,8)
+	defer unix.Flock(int(counterFile.Fd()), unix.LOCK_UN)
+
+	buf := make([]byte, 8)
 	var currentCount uint64
 
-	counterFile.Seek(0,0)
-	_,err=counterFile.Read(buf)
-	
-	binary.Decode(buf,binary.LittleEndian,&currentCount)
-	if err!=nil{
-		return 0,fmt.Errorf("%w",err)
-	}
-	nextCount:=currentCount+1
-	binary.Encode(buf,binary.LittleEndian,nextCount)
+	counterFile.Seek(0, 0)
+	_, err = counterFile.Read(buf)
 
-	counterFile.Seek(0,0)
-	_,err=counterFile.Write(buf)
-
-	if err!=nil{
-		return currentCount,fmt.Errorf("%w",err)
+	binary.Decode(buf, binary.LittleEndian, &currentCount)
+	if err != nil {
+		return 0, fmt.Errorf("%w", err)
 	}
-	
-	return currentCount,nil
+	nextCount := currentCount + 1
+	binary.Encode(buf, binary.LittleEndian, nextCount)
+
+	counterFile.Seek(0, 0)
+	_, err = counterFile.Write(buf)
+
+	if err != nil {
+		return currentCount, fmt.Errorf("%w", err)
+	}
+
+	return currentCount, nil
 }
