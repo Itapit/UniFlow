f178ccd03ed3d395c557c10833f8e65e7d241081
Ronen <ronenshot2006@gmail.com>
2026-09-20 12:59:04 +0300

    feature: creating the config file with all the common varibles for the session manager.

---
+++ session-manager/src/config.py
@@ -0,0 +1,11 @@
+RECEIVERS_CONFIG = {
+    1: {"listen": "127.0.0.1:1400"},
+    2: {"listen": "127.0.0.1:1401"},
+    3: {"listen": "127.0.0.1:1402"},
+}
+
+SESSION_SOCKET_PATH = "/tmp/uniflow_session.sock"
+RS_HELPER_SOCKET_PATH = "/tmp/uniflow_rs_helper.sock"
+
+RECEIVER_BINARY = "./bin/receiver.exe"
+RS_HELPER_BINARY = "./bin/rs_helper.exe"
