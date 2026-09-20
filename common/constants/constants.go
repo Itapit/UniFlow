195f9d5d0072c479d5a35a4aaaafc6d00b65517d
Ronen <ronenshot2006@gmail.com>
2026-09-20 12:17:09 +0300

    little copies.

---
+++ common/constants/constants.go
@@ -0,0 +1,11 @@
+package constants
+
+const (
+	BlockSize           = 134400
+	DefaultDataShrads   = 100
+	DefaultParityShards = 50
+	MaxShardSize        = 1344
+	MaxBlockSize        = DefaultDataShrads * MaxShardSize
+	ChunkSize           = 134400
+	CounterFilePath     = "../../../shared/counter/"
+)
