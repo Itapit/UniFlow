ff2adc6817c9eb1cbf1afe344e641cb8cedc070d
Ronen <ronenshot2006@gmail.com>
2026-09-20 17:21:03 +0300

    feature: computes the hash on the rx machine and checks if its matchs the hash sent from the tx machine. Already tested.

--- session-manager/src/file_tracker.py
+++ session-manager/src/file_tracker.py
@@ -13,25 +13,32 @@ class FileTracker:
 
     # just tracks all the file by the file hash and checks if all the blocks had arraived already so it can set it as complete.
 
+   
     def __init__(self, on_file_complete):
         self._on_file_complete = on_file_complete
         self._lock = threading.Lock()
-        self._blocks = {}          # file_hash -> {block_id: block_bytes}
+        self._blocks = {}
+        self._receivers_seen = {}   # file_hash -> set of receiver_ids across all its blocks
         self._completed_files = set()
 
     def handle_block_assembled(self, file_hash, block_id, total_blocks, block_bytes, contributing_receivers):
         with self._lock:
             if file_hash in self._completed_files:
-                return  # already finished and handed off
+                return
 
             blocks = self._blocks.setdefault(file_hash, {})
             blocks[block_id] = block_bytes
+
+            receivers_seen = self._receivers_seen.setdefault(file_hash, set())
+            receivers_seen.update(contributing_receivers)
 
             if len(blocks) < total_blocks:
                 return
 
             self._completed_files.add(file_hash)
             del self._blocks[file_hash]
+            del self._receivers_seen[file_hash]
 
         ordered_bytes = b"".join(blocks[i] for i in range(total_blocks))
-        self._on_file_complete(file_hash=file_hash, total_blocks=total_blocks, file_bytes=ordered_bytes)
+        self._on_file_complete(file_hash=file_hash, total_blocks=total_blocks,
+                                file_bytes=ordered_bytes, contributing_receivers=receivers_seen)
