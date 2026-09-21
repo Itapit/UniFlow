3342ce1ca9d8cf13243e4b1ea433f3f6f2bd09d4
Itapit <itamardavid90@gmail.com>
2026-09-22 00:28:22 +0300

    Auto stash before merge of "main" and "origin/main"

--- senders/internal/counter/check-file.go
+++ senders/internal/counter/check-file.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"senders/internal/constants"
 
@@ -17,6 +18,9 @@ const (
 )
 
 func InitCounterFile(path string) (*os.File, error) {
+	if err := os.MkdirAll(filepath.Dir(path), 0755); err != nil {
+		return nil, fmt.Errorf("failed to create directory for counter file: %w", err)
+	}
 	file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_EXCL, constants.DefaultFileMode)
 	if err == nil {
 		_ = unix.Flock(int(file.Fd()), unix.LOCK_EX)
